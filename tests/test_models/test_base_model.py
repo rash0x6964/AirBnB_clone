b4ba7eabd8ab2e26fe091c26561903f081f11aa4
Wikidips <wikidips18@gmail.com>
2024-03-09 18:20:24 +0100

    tests added

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,68 @@
+import unittest
+import uuid
+from datetime import datetime, timedelta
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test BaseModel class"""
+    
+    def test_init(self):
+        """Tests initialization of attributes."""
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+        self.assertTrue(uuid.UUID(model.id))
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_init_with_valid_datetime_kwargs(self):
+        """Tests initialization with valid datetime keyword arguments."""
+        now = datetime.now()
+        past_time = now - timedelta(days=1)
+        future_time = now + timedelta(days=1)
+        kwargs = {
+            "created_at": past_time.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+            "updated_at": future_time.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+        }
+        model = BaseModel(**kwargs)
+        self.assertEqual(model.created_at, past_time)
+        self.assertEqual(model.updated_at, future_time)
+
+    def test_init_with_invalid_datetime_format(self):
+        """Tests initialization with invalid datetime format in kwargs."""
+        invalid_kwargs = {"created_at": "invalid_date_format"}
+        with self.assertRaises(ValueError):
+            BaseModel(**invalid_kwargs)
+
+    def test_str(self):
+        """Tests string representation."""
+        model = BaseModel()
+        expected_str = (
+            f"[{model.__class__.__name__}]"
+            f"({model.id}) {model.__dict__}"
+        )
+        self.assertEqual(str(model), expected_str)
+
+    def test_save(self):
+        """Tests updating updated_at timestamp."""
+        model = BaseModel()
+        initial_updated_at = model.updated_at
+        model.save()
+        self.assertGreater(model.updated_at, initial_updated_at)
+
+    def test_to_dict(self):
+        """Tests conversion to dictionary."""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], model.__class__.__name__)
+        self.assertEqual(model_dict['id'], model.id)
+        self.assertIsInstance(model_dict['created_at'], str)
+        self.assertIsInstance(model_dict['updated_at'], str)
+        self.assertTrue(all(
+            field in model_dict for field in ['id', 'created_at', 'updated_at']
+        ))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_place.py
@@ -0,0 +1,72 @@
+from models.place import Place
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.base_model import BaseModel
+import unittest
+
+
+class TestPlace(unittest.TestCase):
+    """Test Place class"""
+
+    def setUp(self):
+        """SetUp the Place class"""
+        self.place = Place()
+        self.user = User()
+        self.city = City()
+        self.amenity = Amenity()
+
+    def test_init(self):
+        """Tests that the Place class is initialized correctly."""
+        self.assertEqual(self.place.city_id, "")
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.name, "")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, [])
+
+    def test_assigning(self):
+        """Tests that attribute get assigned correctly."""
+        city_id_value = self.city.id
+        user_id_value = self.user.id
+        name_value = "name_value"
+        description_value = "<@@##$$"
+        number_rooms_value = 201
+        number_bathrooms_value = 45
+        max_guest_value = 4656
+        price_by_night_value = 234
+        latitude_value = 23.605
+        longitude_value = 34.857
+        amenity_ids_value = [self.amenity.id]
+
+        self.place.city_id = city_id_value
+        self.place.user_id = user_id_value
+        self.place.name = name_value
+        self.place.description = description_value
+        self.place.number_rooms = number_rooms_value
+        self.place.number_bathrooms = number_bathrooms_value
+        self.place.max_guest = max_guest_value
+        self.place.price_by_night = price_by_night_value
+        self.place.latitude = latitude_value
+        self.place.longitude = longitude_value
+        self.place.amenity_ids = amenity_ids_value
+
+        self.assertEqual(self.place.city_id, city_id_value)
+        self.assertEqual(self.place.user_id, user_id_value)
+        self.assertEqual(self.place.name, name_value)
+        self.assertEqual(self.place.description, description_value)
+        self.assertEqual(self.place.number_rooms, number_rooms_value)
+        self.assertEqual(self.place.max_guest, max_guest_value)
+        self.assertEqual(self.place.price_by_night, price_by_night_value)
+        self.assertEqual(self.place.latitude, latitude_value)
+        self.assertEqual(self.place.longitude, longitude_value)
+        self.assertEqual(self.place.amenity_ids, amenity_ids_value)
+
+
+if __name__ == '__main__':
+    unittest.main()
